d38a323cab274efebe61c5df444353ece357b285
wallacese <wallace.se@husky.neu.edu>
2019-02-15 19:41:27 -0500

    Repoint service question react service from localhost to heroku springboot app

--- src/services/ServiceQuestionService.js
+++ src/services/ServiceQuestionService.js
@@ -7,9 +7,9 @@ export default class ServiceQuestionService {
         return this.instance
     }
     findServiceQuestionById = id =>
-        fetch(`http://localhost:8080/api/questions/${id}`)
+        fetch(`cs4500-sp19-noideainc.herokuapp.com/api/questions/${id}`)
             .then(response => response.json())
     findAllServiceQuestions = () =>
-        fetch("http://localhost:8080/api/questions")
+        fetch("cs4500-sp19-noideainc.herokuapp.com/api/questions")
             .then(response => response.json())
 }
