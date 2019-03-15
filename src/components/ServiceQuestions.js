1ac3780f077982fa93b79f2cf93f89a10d2b67d6
wallacese <wallace.se@husky.neu.edu>
2019-03-15 16:47:48 -0400

    Add CRUD operations to Service Question summary page
    Added CRUD functionality for service questions. Needed to update the
    service question front end service to include functions to send requests
    to the backend endpoints for create, update, and delete. Added first,
    rudimentary cut of front end design.

--- src/components/ServiceQuestions.js
+++ src/components/ServiceQuestions.js
@@ -5,6 +5,12 @@ class ServiceQuestions extends React.Component {
         super(props)
         this.serviceQuestionService = ServiceQuestionService.getInstance()
         this.state = {
+            questionForm: {
+                title: '',
+                question: '',
+                type: '',
+                choices: ''
+            },
             serviceQuestions: []
         }
     }
@@ -17,17 +23,119 @@ class ServiceQuestions extends React.Component {
                 })
             )
     }
+
+    deleteServiceQuestion = (id) =>
+        this.serviceQuestionService
+            .deleteQuestion(id);
+
+    createServiceQuestion = (question) =>
+        this.serviceQuestionService
+            .createQuestion(question);
+
+    updateServiceQuestion = (id, question) =>
+        this.serviceQuestionService
+            .updateQuestion(id, question);
+
+    handleTitleChange = (event) =>
+        this.setState({
+            questionForm: {
+                title: event.target.value,
+                question: this.state.questionForm.question,
+                type: this.state.questionForm.type,
+                choices: this.state.questionForm.choices
+            }
+        });
+
+    handleQuestionChange = (event) =>
+        this.setState({
+            questionForm: {
+                title: this.state.questionForm.title,
+                question: event.target.value,
+                type: this.state.questionForm.type,
+                choices: this.state.questionForm.choices
+            }
+        });
+
+    handleTypeChange = (event) =>
+        this.setState({
+            questionForm: {
+                title: this.state.questionForm.title,
+                question: this.state.questionForm.question,
+                type: event.target.value,
+                choices: this.state.questionForm.choices
+            }
+        });
+
+    handleChoicesChange = (event) =>
+        this.setState({
+            questionForm: {
+                title: this.state.questionForm.title,
+                question: this.state.questionForm.question,
+                type: this.state.questionForm.type,
+                choices: event.target.value
+            }
+        });
+
+    updateQuestions = () => this.serviceQuestionService
+                                .findAllServiceQuestions()
+                                .then(serviceQuestions =>
+                                    this.setState({
+                                        serviceQuestions: serviceQuestions
+                                    }))
+
+
     render() {
         return(
             <div>
                 <h3>Service Questions</h3>
+                <form>
+                        <label>
+                            <input type="text" name="title" placeholder={"Title"} value={this.state.questionForm.title} onChange={this.handleTitleChange}/>
+                        </label>
+                        <label>
+                            <input type="text" name="question" placeholder={"Question Text"} value={this.state.questionForm.question} onChange={this.handleQuestionChange}/>
+                        </label>
+                        <label>
+                            <input type="text" name="type" placeholder={"Question Type"} value={this.state.questionForm.type} onChange={this.handleTypeChange}/>
+                        </label>
+                        <label>
+                            <input type="text" name="choices" placeholder={"Question Choices"} value={this.state.questionForm.choices} onChange={this.handleChoicesChange}/>
+                        </label>
+                        <button type="button" onClick={() =>
+                                this.createServiceQuestion(this.state.questionForm)
+                                    .then(this.updateQuestions)
+                                }>
+                            CREATE NEW QUESTION
+                        </button>
+                </form>
                 <table className="table">
                     <tbody>
                     {
                         this.state.serviceQuestions
                             .map(serviceQuestion =>
                                 <tr key={serviceQuestion.id}>
+                                    <td>{serviceQuestion.title}</td>
                                     <td>{serviceQuestion.question}</td>
+                                    <td>{serviceQuestion.type}</td>
+                                    <td>{serviceQuestion.choices}</td>
+                                    {/* DELETE button, calls delete function then updates state*/}
+                                    <td>
+                                        <button onClick={() =>
+                                            this.deleteServiceQuestion(serviceQuestion.id)
+                                                .then(this.updateQuestions)
+                                        }>
+                                            DELETE
+                                        </button>
+                                    </td>
+                                    {/* UPDATE button, calls update function then updates state*/}
+                                    <td>
+                                        <button onClick={() =>
+                                            this.updateServiceQuestion(serviceQuestion.id, this.state.questionForm)
+                                                .then(this.updateQuestions)
+                                        }>
+                                            UPDATE
+                                        </button>
+                                    </td>
                                 </tr>
                             )
                     }
