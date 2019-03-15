1ae577ba3af427074ba962e8937097790133f6bc
Brian Hughes <brianhughes@Brians-MacBook-Pro.local>
2019-03-14 21:42:58 -0400

    improves service answer details page

--- src/components/ServiceAnswerDetails.js
+++ src/components/ServiceAnswerDetails.js
@@ -6,58 +6,94 @@ class ServiceAnswerDetails extends React.Component {
         this.serviceAnswerService = ServiceAnswerService.getInstance()
         this.state = {
             serviceAnswers: [],
-            serviceAnswer: {
+            answer: {
                 choiceAnswer: '',
-                id: 1
+                id: 1,
+                trueFalseAnswer: null,
+                minAnswer: 0,
+                maxAnswer: 0
             }
         }
     }
     componentDidMount() {
+        this.findAllServiceAnswers()
+    }
+    findAllServiceAnswers = () =>
         this.serviceAnswerService
             .findAllServiceAnswers()
             .then(serviceAnswers => {
                     this.props.history.push("/admin/service-answers/" + serviceAnswers[0].id)
                     this.setState({
                         serviceAnswers: serviceAnswers,
-                        serviceAnswer: serviceAnswers[0]
+                        answer: serviceAnswers[0]
                     })
                 }
             )
-    }
     selectServiceAnswer = id =>
         this.serviceAnswerService
             .findServiceAnswerById(id)
-            .then(serviceAnswer => {
+            .then(answer => {
                     this.props.history.push("/admin/service-answers/" + id)
                     this.setState({
-                        serviceAnswer: serviceAnswer
+                        answer: answer
                     })
                 }
             )
+
+    updateForm = e =>
+        this.setState({
+            answer: {
+                choiceAnswer: e.target.value,
+                trueFalseAnswer: e.target.value,
+                minAnswer: e.target.value,
+                maxAnswer: e.target.value,
+                id: this.state.answer.id
+            }
+        })
+
+    createServiceAnswer = () =>
+        this.serviceAnswerService
+            .createServiceAnswer(this.state.answer)
+            .then(this.findAllServiceAnswers())
+    deleteServiceAnswer = id =>
+        this.serviceAnswerService
+            .deleteServiceAnswer(id)
+            .then(this.findAllServiceAnswers())
+    updateServiceAnswer = () =>
+        this.serviceAnswerService.updateServiceAnswer(this.state.answer)
     render() {
         return(
             <div>
                 <h3>Service Answer Details</h3>
                 <select
-                    value={this.state.serviceAnswer.id}
+                    value={this.state.answer.choiceAnswer}
                     onChange={(e) => this.selectServiceAnswer(e.target.value)}
                     className="form-control">
                     {
                         this.state.serviceAnswers
-                            .map(serviceAnswer =>
+                            .map(answer =>
                                 <option
-                                    value={serviceAnswer.id}
-                                    key={serviceAnswer.id}>
-                                    {serviceAnswer.id}
+                                    value={answer.id}
+                                    key={answer.id}>
+                                    {answer.id}
                                 </option>
                             )
                     }
                 </select>
-                <label>Service Answer Answer</label><br/>
+                <label>Answer</label><br/>
                 <input
-                    onChange={() => {}}
+                    onChange={e => this.updateForm(e)}
                     className="form-control"
-                    value={this.state.serviceAnswer.choiceAnswer}/>
+                    value={this.state.answer.id}/>
+                <button onClick={this.createServiceAnswer}>
+                    Create
+                </button>
+                <button onClick={() => this.createServiceAnswer(this.state.answer.id)}>
+                    Delete
+                </button>
+                <button onClick={this.updateServiceAnswer}>
+                    Update
+                </button>
             </div>
         )
     }
--- src/services/ServiceAnswerService.js
+++ src/services/ServiceAnswerService.js
@@ -7,9 +7,35 @@ export default class ServiceAnswerService {
         return this.instance
     }
     findServiceAnswerById = id =>
-        fetch("https://cs4500-sp19-noideainc.herokuapp.com/api/service-answers/${id}")
+        fetch(`https://cs4500-sp19-noideainc.herokuapp.com/api/service-answers/${id}`)
             .then(response => response.json())
     findAllServiceAnswers = () =>
         fetch("https://cs4500-sp19-noideainc.herokuapp.com/api/service-answers/")
             .then(response => response.json())
+
+    createServiceAnswer = answer => {
+        delete answer.id;
+        return fetch(`https://cs4500-sp19-noideainc.herokuapp.com/api/service-answers/`, {
+            method: 'post',
+            body: JSON.stringify(answer),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+    }
+
+    updateServiceAnswer= answer =>
+        fetch(`https://cs4500-sp19-noideainc.herokuapp.com/api/service-answers/${answer.id}`, {
+            method: 'put',
+            body: JSON.stringify(answer),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(response => response.json())
+
+    deleteServiceAnswer = id =>
+        fetch(`https://cs4500-sp19-noideainc.herokuapp.com/api/service-answers/${id}`, {
+            method: 'delete'
+        })
 }
