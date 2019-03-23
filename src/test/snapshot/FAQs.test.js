1090ce397fe619a1372aa8ff39ae800653103cce
Ben Romine <elementnumber35@gmail.com>
2019-03-22 23:00:02 -0400

    Snapshot Test to test Initial DOM and event that changes DOM

--- src/test/snapshot/FAQs.test.js
+++ src/test/snapshot/FAQs.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import FAQs from '../../components/FAQs.js';
 import FAQContainer from '../../components/FAQContainer';
 import renderer from 'react-test-renderer';
+import FAQService from '../../services/FAQService';
 
-
-test('test whether or not a list of FAQs renders correctly', () => {
+test('test the initial dom or not a list of FAQs renders correctly', () => {
     const FAQ = renderer.create(<FAQs
         title=""
         question=""
-        faqs={[{"id":1,"title":"background check","question":"How many projects do you experienced?","answers":[]}]}
+        faqs={[]}
         updateTitle={FAQContainer.updateTitle}
         updateQuestion={FAQContainer.updateQuestion}
         createFAQ={FAQContainer.createFAQ}
@@ -19,4 +19,23 @@ test('test whether or not a list of FAQs renders correctly', () => {
 
     let tree = FAQ.toJSON();
     expect(tree).toMatchSnapshot();
+});
+
+const faqService = FAQService.getInstance();
+test('test find all FAQs', () => {
+    return (faqService.findAllFAQs()).then(faqs => {
+        const FAQ = renderer.create(<FAQs
+            title=""
+            question=""
+            faqs={faqs}
+            updateTitle={FAQContainer.updateTitle}
+            updateQuestion={FAQContainer.updateQuestion}
+            createFAQ={FAQContainer.createFAQ}
+            editFAQ={FAQContainer.editFAQ}
+            deleteFAQ={FAQContainer.deleteFAQ}
+            moveToEdit={FAQContainer.moveToEdit}
+            searchButton={FAQContainer.searchButton}/>);
+        let tree = FAQ.toJSON();
+        return expect(tree).toMatchSnapshot();
+    });
 });
