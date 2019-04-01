48ffde94c2325704562695b95e9cc57b9c2832df
Ben Romine <elementnumber35@gmail.com>
2019-04-01 12:36:28 -0400

    Added basic provider page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 //import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Admin from './components/Admin'
 import ServiceCategories from './components/ServiceCategories'
 import ServiceCategoryDetails from './components/ServiceCategoryDetails'
+import users from './test/MockData/Users.mock'
+import Provider from './components/Provider/Provider'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <h1>No Idea Inc</h1>
-        <Router>
-          <div>
-            <Link to="/admin">Admin</Link>
-            <Route path="/admin" component={Admin}/>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>No Idea Inc</h1>
+                <Router>
+                    <div>
+                        <Link to="/admin">Admin</Link>
+                        <Link to="/provider">Provider</Link>
+                        <Route path="/provider" exact
+                               render={() => <Provider provider={users[0]}/>}/>
+                        <Route path="/admin" component={Admin}/>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
