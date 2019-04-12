1e3ea295844617a23226fd2c2c5f46d83dc54df1
duoduo0921 <wang.hand@husky.neu.edu>
2019-04-11 22:57:01 -0400

    changed the router

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Admin from './components/Admin'
-import ServiceCategories from './components/ServiceCategories'
-import ServiceCategoryDetails from './components/ServiceCategoryDetails'
 import users from './test/MockData/Users.mock'
 import Provider from './components/Provider/Provider'
 import Home from './components/Home'
 import RegisterContainer from './components/Signup/RegisterContainer'
 import ServiceNavigatorContainer from './components/ServiceNavigator/ServiceNavigatorContainer'
-import ServiceProviderNavigator from './components/SearchProviders/ServiceProviderNavigator'
 import ServiceProviderNavigatorContainer from './components/SearchProviders/ServiceProviderNavigatorContainer'
 import LoginContainer from './components/Login/LoginContainer'
 
 class App extends Component {
-
+  
   render() {
     return (
       <div className="container-fluid home">
@@ -32,12 +29,8 @@ class App extends Component {
             <Link to="/login"> Login</Link>
             <br/>
             <Route path="/admin" component={Admin}/>
-            <Route path="/services" exact component={ServiceNavigatorContainer}/>
-            <Route path="/provider" exact render={() => <Provider provider={users[0]}/>}/>
-            {/* <Route
-                path="/provider"
-                exact
-                render={() => <Provider provider={serviceCategories[0].serviceProviders[0]}/>}/> */}
+            <Route path="/services" component={ServiceNavigatorContainer}/>
+            <Route path="/provider" render={() => <Provider provider={users[0]}/>}/>
             <Route
                 path="/home"
                 exact
@@ -45,11 +38,9 @@ class App extends Component {
             <Route path="/register" component={RegisterContainer}/>
             <Route
                 path="/providers"
-                exact
                 component={ServiceProviderNavigatorContainer}/>
             <Route
                 path="/login"
-                exact
                 component={LoginContainer}/>
           </div>
         </Router>
