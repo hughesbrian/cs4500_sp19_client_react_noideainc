43be32e4c4bcf35a41ae629417924fa1f0101005
duoduo0921 <wang.hand@husky.neu.edu>
2019-04-17 23:44:02 -0400

    deleted some console logs

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import ServiceCategoryTabsContainer from './ServiceCategoryTabs/ServiceCategoryT
 
 class Home extends React.Component {
     constructor(props) {
-        console.log(props.LogComponent)
+        //console.log(props.LogComponent)
         super(props);
         this.state = {
             LogComponent: props.LogComponent
